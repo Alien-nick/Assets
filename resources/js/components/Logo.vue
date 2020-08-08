4d878eff3faadd81083fe3f769c9ef538b2f6912
Nicholas Seetaram <nicholas@intellectstorm.com>
2020-08-08 19:58:48 -0400

    Add logo

--- resources/js/components/Logo.vue
+++ resources/js/components/Logo.vue
@@ -1,5 +1,41 @@
 <template>
-    <h1 class="font-normal text-3xl text-grey-darkest leading-none">Full Logo</h1>
+
+<div>    
+    <div class="mb-10">
+            <h1 class="font-normal text-3xl text-grey-darkest leading-none mb-8">Full Logo</h1>
+        <div>
+            <picture style="height:382px;" class="block shadow flex items-center justify-center mb-6">
+                <img src="/images/logo.svg" alt="logo" srcset="">
+            </picture>
+        </div>
+
+        
+        <div class="flex justify-between items-center">
+            <p>Original Logo on <strong>white</strong> background</p>
+
+            <a href="/images/logo.svg" class="border border-gray px-8 py-2 rounded-full text-black font-bold text-xs">logo.svg</a>
+        </div>
+
+    </div>
+
+    <div class="mb-10">
+            <h1 class="font-normal text-3xl text-grey-darkest leading-none mb-8">Full Logo</h1>
+        <div>
+            <picture style="height:382px;" class="block shadow flex items-center justify-center mb-6 bg-black">
+                <img src="/images/logo.svg" alt="logo" srcset="">
+            </picture>
+        </div>
+
+        
+        <div class="flex justify-between items-center">
+            <p>Negative Logo on <strong>white</strong> background</p>
+
+            <a href="/images/logo.svg" class="border border-gray px-8 py-2 rounded-full text-black font-bold text-xs">logo.svg</a>
+        </div>
+
+    </div>
+</div>
+    
 </template>
 
 <script>
