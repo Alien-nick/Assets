c50cc56d298a54ad0199de2685ab816ec3347bc6
Nicholas Seetaram <nicholas@intellectstorm.com>
2020-08-09 07:54:17 -0400

    Import axios

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
+import axios from 'axios';
 import routes from './routes';
+import VueRouter from 'vue-router';
+
+window.axios = axios;
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 Vue.use(VueRouter);
 
